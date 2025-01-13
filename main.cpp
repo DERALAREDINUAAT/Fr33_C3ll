9100dde1051e8f0ba32f4e263356d06aefb398b3
DERALAREDINUAAT <dariaradut47@gmail.com>
2025-01-13 15:35:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Mouse.hpp>
@@ -7,34 +8,45 @@
 #include <sstream>
 #include <fstream>
 
+#include "Exceptii.h"
 #include "Carte.h"
 #include "Selectie.h"
 #include "Tabla.h"
 
 int main()
 {
-    //test afisare carte
-    std::cout << "Test afisare carte\n";
-    carte kk(0, 11);
-    std::cout << kk << '\n';
-    stabileste_dificultatea st;
-    st.afiseaza_init();
-    st.joaca_init();
-    int df = st.da_dificultate();
-    qwk joc(df);
-    //amesteca pachetul de carti
-    srand(time(0));
+    //definire eroare de incarcare
+    try {
+        //test afisare carte
+        std::cout << "Test afisare carte\n";
+        carte kk(0, 11);
+        std::cout << kk << '\n';
+        stabileste_dificultatea st;
+        st.afiseaza_init();
+        st.joaca_init();
+        int df = st.da_dificultate();
+        difficulty2 *dtest;
+        difficulty2 *dClona = dtest->Clone();
+        qwk joc(df);
+        //amesteca pachetul de carti
+        srand(time(0));
 
-    //std::cout << "Afiseaza tabla de joc\n";
-    joc.afiseaza_tabla(0);
+        //std::cout << "Afiseaza tabla de joc\n";
+        joc.afiseaza_tabla(0);
 
-    //modul de lucru se defineste prin valorile
-    // 's' - selecteaza cartea pe care se face clic
-    // 'm' - muta cartea selectata pe pozitia pe care s-a facut clic
-    char mod_lucru = 's';
+        //modul de lucru se defineste prin valorile
+        // 's' - selecteaza cartea pe care se face clic
+        // 'm' - muta cartea selectata pe pozitia pe care s-a facut clic
+        char mod_lucru = 's';
 
-    int dr = 0;
+        int dr = 0;
 
-    joc.joaca(mod_lucru, dr);
+        joc.joaca(mod_lucru, dr);
+    }
+    catch (std::exception& e) {
+        std::cout << e.what() << '\n';
+        system("pause"); 
+    }
+
     return 0;
 }
